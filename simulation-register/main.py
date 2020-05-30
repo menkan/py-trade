f91a59d68591237a5952799284b5305334a5e62d
menKan <xtz17515@163.com>
2020-05-30 23:55:44 +0800

    change code test

--- simulation-register/main.py
+++ simulation-register/main.py
@@ -2,9 +2,19 @@
 # Created by menkan on 20200528
 # Email: xtz17515@163.com
 
-# pip install requests
-# system command
-# import sys
-# input('Press any key to continue')
-# 。
-# sys.exit('exit system')
+# test 1
+
+# need -> pip install requests
+
+import requests
+
+ORIGINHREF = 'https://www.baidu.com'
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+}
+
+response = requests.get(ORIGINHREF, headers=headers)
+
+print(response.content.decode())
+

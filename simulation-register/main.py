9ccb8b67c9fd30601ad8e3e0053c27e664c16561
menKan <xtz17515@163.com>
2020-05-31 12:22:02 +0800

    change code test

--- simulation-register/main.py
+++ simulation-register/main.py
@@ -1,20 +1,63 @@
 # encoding: utf-8
 # Created by menkan on 20200528
 # Email: xtz17515@163.com
-
-# test 1
-
-# need -> pip install requests
+# @Description: 命令行版中文转英文翻译；
+# 参考： https://blog.csdn.net/weixin_40074227/article/details/104238335
+# need -> pip install requests, PyExecJS
 
 import requests
+import re
+import execjs
 
-ORIGINHREF = 'https://www.baidu.com'
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
-}
+def get_sign(w, g):
+    with open('index.js') as f:
+        jsdata = f.read()
+    ctx = execjs.compile(jsdata)
+    return ctx.call('e', w, g)
 
-response = requests.get(ORIGINHREF, headers=headers)
 
-print(response.content.decode())
+def get_token(result):
+    token = re.findall(r"token: '(.*?)',", result)
+    gtk = re.findall(r"window.gtk = '(.*?)';", result)
+    if token and gtk:
+        return token[0], gtk[0]
 
+
+if __name__ == '__main__':
+    session = requests.session()
+
+    url = 'https://fanyi.baidu.com/'
+
+    response = session.post(url)
+    response = session.post(url)
+
+    token, gtk = get_token(response.text)
+
+    headers = {
+        "Rerer": "https://fanyi.baidu.com/",
+        "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36",
+    }
+    # chinese => english
+    print('输入中文-翻译成英文：\n')
+    word = input("：")
+
+    if not word:
+        print('请输入内容')
+        exit()
+
+    sign = get_sign(word, gtk)
+    data = {
+        "from": 'zh',
+        "to": "en",
+        "query": word,
+        "simple_means_flag": '3',
+        "sign": sign,
+        "token": token,
+    }
+    session.post(url='https://fanyi.baidu.com/langdatect', data={'query': word})
+    res = session.post(url="https://fanyi.baidu.com/v2transapi?from=en&to=jp", data=data, headers=headers)
+    print(res.json()['trans_result']['data'][0]['dst'])
+    # while True:
+    # 可以做成一直循环查内容的
+    # print("Result:", res.json()['trans_result']['data'][0])
